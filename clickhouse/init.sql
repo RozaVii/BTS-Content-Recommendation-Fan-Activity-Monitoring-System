3c7ee24c616ae7ce683af2f3483efdfb15e3fc34
Sophie Kuchapina <122675180+RozaVii@users.noreply.github.com>
2026-02-01 18:07:39 +1000

    Update init.sql

--- clickhouse/init.sql
+++ clickhouse/init.sql
@@ -1,14 +1,21 @@
 CREATE DATABASE IF NOT EXISTS bts;
+SHOW DATABASES;
 
-CREATE TABLE IF NOT EXISTS bts.fan_events (
+CREATE TABLE IF NOT EXISTS bts.fan_events
+(
     event_time DateTime,
-    user_id UInt64,
+    event_id String,
+    user_id UInt32,
     content_id UInt32,
-    content_type String,
-    member_bias String,
-    event_type String,
+    content_type LowCardinality(String),
+    member_bias LowCardinality(String),
+    event_type LowCardinality(String),
     watch_time_sec UInt16,
-    source String,
-    mode String
-) ENGINE = MergeTree
-ORDER BY event_time;
+    source LowCardinality(String),
+    mode LowCardinality(String)
+)
+ENGINE = MergeTree
+PARTITION BY toDate(event_time)
+ORDER BY (event_time, member_bias);
+
+DESCRIBE TABLE bts.fan_events;
